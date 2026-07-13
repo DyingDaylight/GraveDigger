fe15564cbce3b556ae4288a191743229da455320
Ekaterina Gorodetskaia <4414513+DyingDaylight@users.noreply.github.com>
2026-07-13 23:14:48 +0300

    coordinator fix

--- GraveDigger/GameplayCoordinator.cs
+++ GraveDigger/GameplayCoordinator.cs
@@ -15,9 +15,6 @@ public class GameplayCoordinator : IGameplayActions
     private readonly RandomService randomService;
     private readonly LootGenerator lootGenerator;
     private readonly Inventory inventory;
-    
-    public event Action<Tombstone> OnGraveDug;
-    public event Action<Tombstone> OnGraveRepaired;
 
     public event Action<List<ItemData>, Tombstone> OnLootSpawn;
     public event Action<Tombstone> OnGraveDug;
